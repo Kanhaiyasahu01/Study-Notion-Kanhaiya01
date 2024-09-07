84359a6e4ee893023093401dd05560fbb787e057
Kanhaiyasahu01 <kanhaiyasahutools@gmail.com>
2024-09-07 15:08:47 +0530

    all files changed to bcryptjs

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const mailSender = require("../utils/mailSender");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
 exports.resetPasswordToken = async (req, res) => {
